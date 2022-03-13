9348974a846864a5687e6da09737b6dc1fa542a1
Rafael Prado <ra.pcamargo@hotmail.com>
2022-03-13 16:42:41 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,14 @@
+import axios from "axios";
+import { BASE_URL } from "utils/requests";
 import MovieCard from "../../components/MovieCard";
 import Pagination from "../../components/Pagination";
 
 function Listing() {
+
+    axios.get(`${BASE_URL}/movies?size=12&page=0'`)
+        .then(response => {
+            console.log(response.data)
+        })
 
     return (
         <>
@@ -23,7 +30,7 @@ function Listing() {
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div>
-                    
+
                 </div>
             </div>
         </>
